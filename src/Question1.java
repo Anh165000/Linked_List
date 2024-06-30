0745b2a941cd171912c0d82b3f784b0b39b92f49
Anh165000 <phamduyanh.01062005da@gmail.com>
2024-06-30 18:14:15 +0700

    q1 complete

--- src/Question1.java
+++ src/Question1.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class Question1 {
+    public static int list[] = new int[100];
+    public static int n;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public Question1() {
+    }
+
+    public static void insert_foward() {
+        n = Integer.parseInt(scanner.nextLine());
+        while (true) {
+            try {
+                for (int i = 0; i < n; i++) {
+                    list[i] = scanner.nextInt();
+                }
+                break;
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
+    public static void insert_backward() {
+        n = Integer.parseInt(scanner.nextLine());
+        while (true) {
+            try {
+                for (int i = n - 1; i >= 0; i--) {
+                    list[i] = scanner.nextInt();
+                }
+                break;
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
+    public static void print_list() {
+        for(int i = 0; i < n; i++) {
+            System.out.printf("%d ", list[i]);
+        }
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // insert_foward();
+        // insert_backward();
     }
 }
