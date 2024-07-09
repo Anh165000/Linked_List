37aae65426d65a9d3b2c08b1114af1abf6dc53bf
Anh165000 <phamduyanh.01062005da@gmail.com>
2024-07-09 19:36:24 +0700

    completed object Q3b_data, completed method input_list(), building sort...

--- src/Data/Question3b_data.java
+++ src/Data/Question3b_data.java
@@ -1,11 +1,13 @@
 package Data;
 
-public class Question3b_data {
+public class Question3b_data{
 
-    public Question3b_data(){
-        int n;
-        int pointer;
+    public int n;
+    public Question3b_data linked;
+
+    public Question3b_data(int n){
+        this.n = n;
+        //this.linked = null;
     }
-
 
 }
--- src/Main/Question3b_main.java
+++ src/Main/Question3b_main.java
@@ -5,11 +5,45 @@ import Data.Question3b_data;
 
 public class Question3b_main {
 
-    private Scanner sc = new Scanner(System.in);
-    //private Question3b_data q = new Question3b_data();
-    private Question3b_data linkedList[] = new Question3b_data[100];
+    private static Scanner sc = new Scanner(System.in);
+    private static Question3b_data linkedList[];
 
     public static void main(String[] args) {
+        input_list();
+    }
+
+    public static void input_list(){
+        
+        int size = sc.nextInt();
+        linkedList = new  Question3b_data[size];
+        
+        for(int i = 0; i < size; i++){
+            linkedList[i] = new Question3b_data(sc.nextInt());
+        }
+        for(int i = size - 1; i >= 0; i--){
+            if(i < size - 1)
+                linkedList[i].linked = linkedList[i + 1];
+            else
+                linkedList[size - 1].linked = null;
+        }
+        
+        // for(int i = 0; i < size; i++){
+        //     System.out.printf("%d ", linkedList[i].n);
+        // }
+
+        // for(int i = 0; i < size; i++){
+        //     if(linkedList[i].linked != null)
+        //         System.out.println(linkedList[i].linked);
+        //     else
+        //         System.out.printf("%s", "null");
+        // }
+    }
+
+    public static void sort_ascending(){
+
+    }
+
+    public static void sort_descending(){
         
     }
 }
